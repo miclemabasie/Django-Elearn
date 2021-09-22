f14a6f35e1a0db5dfa765e6fa5d7561c1682e074
Miclem Electron <miclemabasie3@gmail.com>
2021-09-22 12:55:11 +0100

    Classes and mixins

--- educa/settings.py
+++ educa/settings.py
@@ -40,7 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'testapps.apps.TestappsConfig',
+    'crispy_forms'
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -57,7 +62,7 @@ ROOT_URLCONF = 'educa.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +126,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'asssets')
+
+
+# MEDIA
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
---
+++ static/bootstrap/js/popover.js
@@ -0,0 +1,7 @@
+var popover = new bootstrap.Popover(document.querySelector('.example-popover'), {
+    container: 'body'
+  })
+
+var popover = new bootstrap.Popover(document.querySelector('.popover-dismiss'), {
+trigger: 'focus'
+})
